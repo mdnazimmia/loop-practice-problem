704a21dc1568a23ca3c0954418572d13d8b88568
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-06 22:57:09 +0200

    previous revise

---
+++ revision.js
@@ -0,0 +1,8 @@
+var fruits = ['apple', 'orange', 'coconut', 'lemon', 'guaba'];
+// console.log(fruits);
+// var fifthOne = fruits[4];
+fruits[3] = 'ananas';
+// console.log(fruits);
+
+var indexItem = fruits.indexOf('coconut');
+console.log(indexItem);
