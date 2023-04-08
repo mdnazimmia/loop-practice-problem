bf424ee13201616804ce304a09c6a91af8f8dfce
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-08 14:34:50 +0200

    odd.js

--- while.js
+++ while.js
@@ -1,5 +1,13 @@
-var rostGiven = 0;
-while (rostGiven > 7) {
-    console.log('rost den pelase')
-    rostGiven++;
+/**
+ * 1. Loog variable
+ * 2. Condition inside while
+ * 3. Loop Body
+ * 4. Change the loop variable
+ */
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Please give me a roast !! ')
+    console.log('roastGiven');
+    roastGiven++;
 }
