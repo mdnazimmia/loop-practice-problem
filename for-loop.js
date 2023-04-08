be4c9d5ab6c048d140fede02911cdc6ca61497cf
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-09 00:34:45 +0200

    for loop

---
+++ for-loop.js
@@ -0,0 +1,24 @@
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     console.log('Please give me a Roast !!');
+//     roastGiven++;
+// }
+
+// for loop
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log('Give me a roast');
+// }
+// // simple version for loop
+// for (var i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+// even for loop
+for (var i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+//odd for loop
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
