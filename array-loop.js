4fb14a2e720ca8f8f60dfb99f317ee53373c8b82
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-10 14:45:09 +0200

    if and while loop

---
+++ array-loop.js
@@ -0,0 +1,20 @@
+// for (var i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+
+// target : display every element of an array
+// var numbers = [43, 30, 78, 89, 39, 88, 78, 30, 80, 78, 38];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var tables = ['laptop', 'monitor', 'keyboard', 'mouse', 'stand', 'flower'];
+
+for (var i = 0; i < tables.length; i++) {
+    var table = tables[i];
+    console.log(table);
+    var index = tables.indexOf('mouse');
+}
---
+++ reminder.js
@@ -0,0 +1,12 @@
+// var number = 119;
+// var reminder = number % 5;
+// console.log(reminder);
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+var bananaIndex = fruits.indexOf('Banana');
+fruits[bananaIndex] = 'Mango'
+console.log(bananaIndex);
+fruits.pop('');
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
